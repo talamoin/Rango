46ab7a35e9f22c1539993c7112c24a371ffbd190
Tala Moin <talamoin@gmail.com>
2019-02-11 22:04:35 +0000

    edit

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -11,23 +11,36 @@ from django.contrib.auth import authenticate, login
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout 
-
+from datetime import datetime
 def index(request):
 	
     #context_dict={'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
     #return HttpResponse("Rango says hey there partner <a href='/rango/about'> Click here to view the about</a>")
     
     #return render(request,'rango/index.html',context=context_dict)
-    
+    request.session.set_test_cookie()
     category_list=Category.objects.order_by('-likes')[:5]
     pages_list=Page.objects.order_by('-views')[:5]
-    context_dict={'categories':category_list,"pages":pages_list}
-    return render(request,'rango/index.html',context=context_dict)
+    context_dict = {'categories': category_list, 'pages': pages_list}
+    
+    visitor_cookie_handler(request)
+    context_dict['visits'] =request.session['visits']
+
+    response=render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
+
+	if request.session.test_cookie_worked():
+		print("TEST COOKIE WORKED!")
+		request.session.delete_test_cookie()
+
+	visitor_cookie_handler(request)
+	context_dict['visits']=request.session['visits']
+
 	print(request.method)
 	print(request.user)
-	return render(request,'rango/about.html',{})
+	return render(request,'rango/about.html',context_dict)
 
 def show_category(request,category_name_slug):
 	context_dict={}
@@ -86,29 +99,24 @@ def add_page (request,category_name_slug):
 	return render(request,'rango/add_page.html',context_dict)
 
 
-def register(request):
+def register (request):
 	registered=False
 	if request.method == 'POST':
 		user_form =UserForm(data=request.POST)
 		profile_form=UserProfileForm(data=request.POST)
-
 		if user_form.is_valid() and profile_form.is_valid():
 			user=user_form.save()
-
 			user.set_password(user.password)
 			user.save()
 
 			profile=profile_form.save(commit=False)
 			profile.user=user
-
 			if 'picture' in request.FILES:
 				profile.picture=request.FILES['picture']
-
 			profile.save()
 			registered=True
-
 		else:
-			print(user_form.errors, profile_form.errors)
+			print(user_form.errors,profile_form.errors)
 	else:
 		user_form=UserForm()
 		profile_form=UserProfileForm()
@@ -143,5 +151,24 @@ def user_logout(request):
 	logout(request)
 	return HttpResponseRedirect(reverse('index'))
 
+def visitor_cookie_handler(request):
+	visits=int(get_server_side_cookie(request,'visits','1'))
+
+	last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
+	last_visit_time=datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+
+	if(datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		request.session['last_visit']=str(datetime.now())
+	else:
+		request.session['last_visit']=last_visit_cookie
+
+	request.session['visits']=visits
+
+def get_server_side_cookie(request,cookie,default_val=None):
+	val=request.session.get(cookie)
+	if not val:
+		val=default_val
+	return val
 
 
